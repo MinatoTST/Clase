7f13052534496c4b29a904adbd0b5066410704f6
Minato <minatotst12@gmail.com>
2020-10-06 09:50:08 +0200

    Nombre completo

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ class My Name {
     public static void main (String args[]){
        System.out.println("Pedro García");
        System.out.println("Nací en Rioseco de Tapia en 2002");
+       System.out.println("Vivo en Azadinos y uso Windows 10");
    }
 }
